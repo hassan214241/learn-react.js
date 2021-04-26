6caa27da35ffe5d7499fb2c7bed04363f2a6ee62
hassan214241 <hassan214241@gmail.com>
2021-04-26 15:40:14 +0500

    navaBar and table assignment

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import react from "react";
+import Bar from "./component/Bar";
+import DisableFieldEditable from "./component/Table"
+
+
+function App(){
+return(
+  <>
+  <Bar/> 
+ <DisableFieldEditable/>
+  
+</>
+);
+
+}
+
+export default App;
